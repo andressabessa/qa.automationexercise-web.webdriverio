919eea190270ba70e988ecf499d92120f7b91742
Andressa Bessa <andreessabessa@gmail.com>
2025-08-22 15:16:01 -0300

    feat: adding user registration tests

---
+++ test/specs/registerUser.spec.js
@@ -0,0 +1,33 @@
+import { HomePage } from '../pageobjects/home.page.js'
+import { LoginPage } from '../pageobjects/login.page.js'
+import { RegisterPage } from '../pageobjects/register.page.js'
+
+const homePage = new HomePage()
+const loginPage = new LoginPage()
+const registerPage = new RegisterPage()
+
+describe('Funcionalidade: Cadastro', () => {
+  beforeEach(async () => {
+    await homePage.open('/')
+  })
+  
+  it('CT01: Deve cadastrar um usuário com sucesso', async () => {
+    // Arrange - O ambiente já foi preparado no beforeEach (navegação para página inicial)
+    
+    // Act - Executar as ações do teste
+    await homePage.clickSignupLogin()
+    await loginPage.fillNameAndEmail()
+    await loginPage.clickButtonSignup()
+    await registerPage.fillPersonalDetails()
+    await registerPage.selectNewsletter()
+    await registerPage.selectSpecialOffers()
+    await registerPage.fillAddressDetails()
+    await registerPage.clickCreateAccount()
+    await registerPage.clickContinue()
+    await registerPage.clickDeleteAccount()
+    
+    // Assert - Verificar se o resultado é o esperado
+    await registerPage.verifyAccountDeleted()
+    await registerPage.clickContinue()
+  })
+})
